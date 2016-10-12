6df3eefde1733c2ce1fe7c30a1a3fe678fb40489
szhou1 <zhousteve3@gmail.com>
2016-10-11 20:29:05 -0700

    Completed OAuth via Github

--- app/models/user.js
+++ app/models/user.js
@@ -8,11 +8,14 @@ var User = db.Model.extend({
   tableName: 'users',
 
   initialize: function() {
+    console.log('initialize user model');
     this.on('creating', function(model, attrs, options) {
+      console.log('creating user in DB');
       model.set('username', model.attributes.username);
-      model.set('password', model.attributes.password);
+      // model.set('password', model.attributes.password);
     });
   }
+
 });
 
 module.exports = User;
--- shortly.js
+++ shortly.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var session = require('express-session');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+// var session = require('express-session');
 var url = require('url');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -23,70 +26,106 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-app.use(session({
-  name: 'server-session-shortly',
-  secret: 'secret_thing',
-  saveUninitialized: true,
-  resave: true,
-  cookie: {
-    maxAge: 10000
-  }
-}));
+app.use(session({secret: 'session secret'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-var authenticate = function(req, res, next) {
+passport.use(new GitHubStrategy({
+  clientID: '3fff66762b75a629c5c1',
+  clientSecret: '77b5bc754e9e66598255eef091e98310f5635700',
+  callbackURL: 'http://127.0.0.1:4568/auth/github/callback'
+},
+  function(accessToken, refreshToken, profile, done) {
+    console.log('inside githubstrategy callback');
+    // console.log('User', User);
+    // console.log('profile', profile);
+    new User({username: profile.username})
+      .fetch()
+      .then( function(model) {
+        var user = model;
+        if (user === null) {
+          new User({
+            username: profile.username
+          }).save()
+          .then(function(model2) {
+            // console.log('AFTER CREATING NEW USER', model2.attributes);
+            return done(null, model2.attributes);
+          });
 
-  if (req.session.isAuth) {
-    console.log('PASSED AUTH');
-    next();
-  } else {
-    console.log('FAILED AUTH');
-    res.redirect('/login');
+        } else {
+          console.log(user.attributes);
+          return done(null, user.attributes);
+        }
+      });
+
   }
+));
+
+passport.serializeUser(function(user, done) {
+  console.log('serializeUser', user);
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  console.log('deserializeUser', username);
+  new User({username: username}).fetch().then( function(model) {
+    // console.log('user', model.attributes);
+    done(null, model.attributes.username);
+  });
+  // done();
+
+});
+
+var ensureAuthenticated = function(req, res, next) {
+  console.log('ensureAuthenticated() req.user', req.user);
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
 };
 
-app.get('/', authenticate, function(req, res) {
-  console.log('get /');
+app.get('/protected', ensureAuthenticated, function(req, res) {
+  console.log('GET /protected');
   res.render('index');
 });
 
-var userCheck = function(user, callback) {
-  var check = false;
+app.get('/auth/github', passport.authenticate('github'));
 
-  db.knex('users')
-    .where('username', '=', user.username)
-    .andWhere('password', '=', user.password)
-    .then(function(dbRes) {
-      if (dbRes && dbRes.length > 0) {
-        if (user.username === dbRes[0].username) {
-          console.log('user check is true');
-          check = true;
-        }
-      }
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/'}), 
+  function(req, res) {
+    console.log('GET /auth/github/callback');
+    res.redirect('/');
+  });
 
-      callback(check);
-    }).catch(function(err) {
-      throw {
-        type: 'DatabaseError',
-        message: 'Failed to get user and password'
-      };
-    });
-};
+
+app.get('/', 
+  function(req, res) {
+    console.log('get /');
+    console.log('req.user', req.user);
+    if (req.isAuthenticated()) {
+      console.log('authenticated');
+      res.render('index');
+    } else {
+      console.log('not authenticated');
+      res.render('landing');
+    }
+  });
+
 
 app.get('/logout', function(req, res) {
   console.log('inside LOGOUT');
-  req.session.destroy(function() {
-    console.log('redirecting to / ');
-    res.redirect('/');  
-  });
+  req.logout();
+  res.render('landing');
+
 });
 
-app.get('/create', authenticate,
+app.get('/create', passport.authenticate('github'),
 function(req, res) {
   console.log('GET /create');
   res.render('index');
 });
 
-app.get('/links', authenticate,
+app.get('/links',
+  // passport.authenticate('github'),
 function(req, res) {
   console.log('GET /links');
 
@@ -95,7 +134,8 @@ function(req, res) {
   });
 });
 
-app.post('/links', authenticate, 
+app.post('/links', 
+  // passport.authenticate('github'), 
 function(req, res) {
   console.log('POST /links');
   var uri = req.body.url;
@@ -131,47 +171,26 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
-app.get('/login', function(req, res) {
+app.get('/login', function(req, res, next) {
   console.log('GET /login');
-  res.render('login');
+  res.redirect('/');
+  // req.login(req.user, function(err) {
+  //   if (err) {
+  //     next(err);
+  //   }
+  //   res.render('login');
+  // });
+
 });
 
-app.post('/login', function(req, res) {
-  console.log('POST /login');
-  // check db for user
-  req.session.user = {
-    username: req.body.username,
-    password: req.body.password
-  };
+app.post('/login', 
+  passport.authenticate('github'), 
+  function(req, res) {
+    console.log('POST /login');
+    res.redirect('/');
 
-  userCheck(req.session.user, function(passed) {
-    req.session.isAuth = passed;
-    console.log('isAuth after userCheck', req.session.isAuth);
-    if (req.session.isAuth) {
-      res.redirect('/');
-    } else {
-      res.redirect('/login');
-    }
   });
-});
 
-app.get('/signup', function(req, res) {
-  console.log('GET /signup');
-  res.render('signup');
-});
-
-app.post('/signup', function(req, res) {
-  console.log('POST /signup');
-  new User({
-    'username': req.body.username,
-    'password': req.body.password
-  }).save()
-  .then(function(model) {
-    console.log('res ending after then');
-    // res.setHeader('Location', '/index');
-    res.redirect('/login');
-  });
-});
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
